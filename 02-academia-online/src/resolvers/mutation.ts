c2dc40f540605bb02f8a842f7cb382a3c38a74c4
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-09-26 23:14:28 -0500

    Terminar api de academia online basado en archivos JSON

--- 02-academia-online/src/resolvers/mutation.ts
+++ 02-academia-online/src/resolvers/mutation.ts
@@ -18,23 +18,72 @@ const mutation: IResolvers = {
         reviews: [],
       };
 
-      if (database.cursos.filter((itemCur) => itemCur.title === itemCurso.title).length === 0) {
+      if (
+        database.cursos.filter((itemCur) => itemCur.title === itemCurso.title)
+          .length === 0
+      ) {
         database.cursos.push(itemCurso);
         return itemCurso;
       } else {
         return {
           id: -1,
           title: `El curso ya existe con este titulo`,
-          description: '',
+          description: "",
           clases: -1,
           time: 0.0,
-          logo: '',
-          level: 'TODOS',
-          path: '',
-          teacher: '',
+          logo: "",
+          level: "TODOS",
+          path: "",
+          teacher: "",
           reviews: [],
         };
       }
+    },
+    modificarCurso(__: void, { curso }): any {
+      const cursoExiste = _.findIndex(database.cursos, (cursoInDex) => {
+        return cursoInDex.id === curso.id;
+      });
+      if (cursoExiste > -1) {
+        const valoraciones = database.cursos[cursoExiste].reviews;
+        curso.reviews = valoraciones;
+        database.cursos[cursoExiste] = curso;
+        return curso;
+      } else {
+        return {
+          id: -1,
+          title: `El curso NO existe en la base de datos`,
+          description: "",
+          clases: -1,
+          time: 0.0,
+          logo: "",
+          level: "TODOS",
+          path: "",
+          teacher: "",
+          reviews: [],
+        };
+      }
+    },
+    eliminarCurso(__: void, { id }): any {
+      const borrarCurso = _.remove(database.cursos, (curso) => {
+        return curso.id === id;
+      });
+
+      if (borrarCurso[0] === undefined) {
+        return {
+          id: -1,
+          title: `El curso NO se puede borrar porque no existe`,
+          description: "",
+          clases: -1,
+          time: 0.0,
+          logo: "",
+          level: "TODOS",
+          path: "",
+          teacher: "",
+          reviews: [],
+        };
+      } 
+
+      return borrarCurso[0]
     },
   },
 };
